51818789f30470e03b2f7143045ae0787972c1ae
michaeluskov <uskovm@gmail.com>
2020-10-22 01:16:06 +0500

    Finished lectures view

--- components/LectureCard/index.tsx
+++ components/LectureCard/index.tsx
@@ -10,7 +10,11 @@ type Props = {
 
 const LectureCard: React.FC<Props> = ({ lecture }) => {
     return <article className={styles.wrapper}>
-        <div className={styles.previewPic} style={{ backgroundImage: `url(${lecture.previewImageUrl})` }} />
+        <div className={styles.previewPic} style={{ backgroundImage: `url(${lecture.previewImageUrl})` }}>
+            {lecture.video?.duration &&
+                <span className={styles.duration}>{lecture.video?.duration}</span>
+            }
+        </div>
         <Shiitake lines={2} className={styles.lectureName} tagName="div">{lecture.name}</Shiitake>
         <div className={styles.lector}>{lecture.lector.name}</div>
     </article>;
